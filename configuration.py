4e9830c42dfcc54eafc196f3e127fa52d031ee46
Aleksander250 <strakhov.aleksandr25@yandex.kz>
2024-12-05 21:49:17 +0600

    Fixed

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://c0fd41d3-a498-45cd-8831-286d7fa4444b.serverhub.praktikum-services.ru/"
+URL_SERVICE = "https://c0fd41d3-a498-45cd-8831-286d7fa4444b.serverhub.praktikum-services.ru"
 
 CREATE_ORDER_PATH = "/api/v1/orders"
 
